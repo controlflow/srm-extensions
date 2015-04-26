3d1e0bad0edb5110c8aadc4348ab4f3cf376e5cd
ControlFlow <shvedov89@gmail.com>
2015-04-27 02:27:38 +0300

    Instructions count method gen

--- src/ConsoleRunner/IlReaderBuilder.cs
+++ src/ConsoleRunner/IlReaderBuilder.cs
@@ -9,83 +9,202 @@ using System.Text.RegularExpressions;
 
 class IlReaderBuilder
 {
-  private readonly StringBuilder myBuilder = new StringBuilder();
-  private readonly HashSet<string> myCases = new HashSet<string>(StringComparer.Ordinal);
+  private readonly StringBuilder myReadMethod = new StringBuilder();
+  private readonly StringBuilder myCountMethod = new StringBuilder();
+  //private readonly HashSet<string> myCases = new HashSet<string>(StringComparer.Ordinal);
+  private readonly List<OpCode> myAllOpcodes;
 
-  const byte MultiByteOpCodePrefix = 0xFE;
-
-  public string Build()
+  public IlReaderBuilder()
   {
-    var allOpcodes = typeof(OpCodes).GetFields()
+    myAllOpcodes = typeof(OpCodes).GetFields()
       .Where(x => x.FieldType == typeof(OpCode))
       .Select(x => (OpCode)x.GetValue(null))
       .ToList();
-
-    myBuilder.AppendLine("switch (reader.ReadByte()) {");
-
-    // single-byte instructions
-    foreach (var code in allOpcodes)
-    {
-      var upperByte = code.Value >> 8;
-      if ((upperByte & MultiByteOpCodePrefix) != 0) continue;
-
-      myBuilder.AppendFormat("case 0x{0:X2}: // {1}", code.Value, code.Name).AppendLine();
-
-      AppendOpcode(code);
-      myBuilder.AppendLine(" continue;");
-    }
-
-    myBuilder.AppendFormat("case {0}:", MultiByteOpCodePrefix).AppendLine();
-    myBuilder.AppendLine("  switch (reader.ReadByte()) {");
-
-    // two-byte instructions
-    foreach (var code in allOpcodes)
-    {
-      var upperByte = code.Value >> 8;
-      if ((upperByte & MultiByteOpCodePrefix) == 0) continue;
-
-      var value = code.Value & byte.MaxValue;
-      myBuilder.AppendFormat("  case 0x{0:X2}: // {1}", value, code.Name).AppendLine();
-      myBuilder.Append("  ");
-
-      AppendOpcode(code);
-      myBuilder.AppendLine("  continue;");
-    }
-
-    myBuilder.AppendLine("  default:");
-    myBuilder.AppendLine("    UnexpectedOpcode();");
-    myBuilder.AppendLine("    continue;");
-    myBuilder.AppendLine("  }");
-    //myBuilder.AppendLine("  break;");
-
-    myBuilder.AppendLine("default:");
-    myBuilder.AppendLine("  UnexpectedOpcode();");
-    myBuilder.AppendLine("  continue;");
-
-    myBuilder.AppendLine("}");
-
-    var cases = string.Join(", ", myCases.OrderBy(x => x).Select((x, i) => string.Format("{0} = {1}", x, i)));
-
-    myBuilder.AppendFormat("enum Opcode {{ {0} }}", cases);
-
-    return myBuilder.ToString();
   }
 
-  private void AppendOpcode(OpCode code)
+  const byte MultiByteOpCodePrefix = 0xFE;
+
+  public string BuildReadMethod()
+  {
+    var readMethod = myReadMethod;
+    readMethod.AppendLine("switch (reader.ReadByte()) {");
+
+    // single-byte instructions
+    foreach (var code in myAllOpcodes)
+    {
+      if (IsTwoByteOpcode(code)) continue;
+      if (code.Value == MultiByteOpCodePrefix) continue;
+
+      readMethod.AppendFormat("case 0x{0:X2}: // {1}", code.Value, code.Name).AppendLine();
+
+      ReadOperand(code, readMethod);
+      readMethod.AppendLine(" continue;");
+    }
+
+    readMethod.AppendFormat("case {0}:", MultiByteOpCodePrefix).AppendLine();
+    readMethod.AppendLine("  switch (reader.ReadByte()) {");
+
+    // two-byte instructions
+    foreach (var code in myAllOpcodes)
+    {
+      if (!IsTwoByteOpcode(code)) continue;
+
+      var value = code.Value & byte.MaxValue;
+      readMethod.AppendFormat("  case 0x{0:X2}: // {1}", value, code.Name).AppendLine();
+      readMethod.Append("  ");
+
+      ReadOperand(code, readMethod);
+      readMethod.AppendLine("  continue;");
+    }
+
+    // todo: remove?
+    readMethod.AppendLine("  default:");
+    readMethod.AppendLine("    UnexpectedOpcode();");
+    readMethod.AppendLine("    continue;");
+    readMethod.AppendLine("  }");
+
+    readMethod.AppendLine("default:");
+    readMethod.AppendLine("  UnexpectedOpcode();");
+    readMethod.AppendLine("  continue;");
+    readMethod.AppendLine("}");
+
+    //var cases = string.Join(", ", myCases.OrderBy(x => x).Select((x, i) => string.Format("{0} = {1}", x, i)));
+    //myReadMethod.AppendFormat("enum Opcode {{ {0} }}", cases);
+
+    return readMethod.ToString();
+  }
+
+  private static bool IsTwoByteOpcode(OpCode code)
+  {
+    var upperByte = code.Value >> 8;
+    return (upperByte & MultiByteOpCodePrefix) != 0;
+  }
+
+  public string BuildCountMethod()
+  {
+    var countMethod = myCountMethod;
+    countMethod.AppendLine("switch (reader.ReadByte()) {");
+
+    CountOpcodes(myAllOpcodes.Where(code => !IsTwoByteOpcode(code)));
+
+    countMethod.AppendFormat("case {0}:", MultiByteOpCodePrefix).AppendLine();
+
+    {
+      countMethod.AppendLine("  switch (reader.ReadByte()) {");
+
+      CountOpcodes(myAllOpcodes.Where(IsTwoByteOpcode));
+
+      countMethod.AppendLine("  default:");
+      countMethod.AppendLine("    UnexpectedOpcode();");
+      countMethod.AppendLine("    continue;");
+      countMethod.AppendLine("  }");
+    }
+
+    countMethod.AppendLine("default:");
+    countMethod.AppendLine("  UnexpectedOpcode();");
+    countMethod.AppendLine("  continue;");
+    countMethod.AppendLine("}");
+
+    countMethod.AppendLine("}");
+
+    return countMethod.ToString();
+  }
+
+  private void CountOpcodes(IEnumerable<OpCode> allOpcodes)
+  {
+    foreach (var groupByOperandType in allOpcodes.GroupBy(x => SkipOperandCode(x.OperandType)).OrderByDescending(x => string.IsNullOrEmpty(x.Key)))
+    {
+      foreach (var code in groupByOperandType.OrderBy(x => x.Value))
+      {
+        var value = code.Value;
+        if (value == MultiByteOpCodePrefix) continue;
+
+        if (IsTwoByteOpcode(code)) value = (short) (value & byte.MaxValue);
+
+        myCountMethod.AppendFormat("case 0x{0:X2}: // {1}", value, code.Name).AppendLine();
+      }
+
+      myCountMethod.Append("  ");
+      myCountMethod.Append(groupByOperandType.Key);
+      myCountMethod.AppendLine();
+      myCountMethod.AppendLine("  continue;");
+    }
+  }
+
+  private static string SkipOperandCode(OperandType operandType)
+  {
+    switch (operandType)
+    {
+      case OperandType.InlineField:
+      case OperandType.InlineI:
+      case OperandType.InlineMethod:
+      case OperandType.InlineSig:
+      case OperandType.InlineString:
+      case OperandType.InlineTok:
+      case OperandType.InlineType:
+      case OperandType.ShortInlineR:
+      {
+        return "reader.ReadInt32();";
+      }
+
+      case OperandType.InlineSwitch:
+      {
+        return "for (var cases = reader.ReadUInt32(); cases > 0; cases--) reader.ReadInt32();";
+      }
+
+      case OperandType.InlineI8:
+      case OperandType.InlineR:
+      {
+        return "reader.ReadInt64();";
+      }
+
+      case OperandType.InlineVar:
+      {
+        return "reader.ReadInt16();";
+      }
+
+      case OperandType.ShortInlineVar:
+      case OperandType.ShortInlineI:
+      {
+        return "reader.ReadByte();";
+      }
+
+      // todo: add decoding?
+      case OperandType.InlineBrTarget:
+      {
+        return "reader.ReadInt32();";
+      }
+
+      case OperandType.ShortInlineBrTarget:
+      {
+        return "reader.ReadSByte();";
+      }
+
+      case OperandType.InlineNone:
+      {
+        return string.Empty;
+      }
+
+      default:
+        throw new ArgumentOutOfRangeException();
+    }
+  }
+
+  private static void ReadOperand(OpCode code, StringBuilder builder)
   {
     var name = code.Name;
     var value = GetValue(code, ref name);
 
-    myBuilder.AppendFormat("  instructions.Add(new Instruction(offset");
+    builder.AppendFormat("  instructions.Add(new Instruction(offset");
 
     var caseName = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name).Replace(".", "");
-    myBuilder.AppendFormat(", Opcode.{0}", caseName);
+    builder.AppendFormat(", Opcode.{0}", caseName);
 
-    myCases.Add(caseName);
+    //myCases.Add(caseName);
 
     if (value != null)
     {
-      myBuilder.Append(", ").Append(value.Value);
+      builder.Append(", ").Append(value.Value);
 
       Debug.Assert(code.OperandType == OperandType.InlineNone);
     }
@@ -102,52 +221,52 @@ class IlReaderBuilder
         case OperandType.InlineType:
         case OperandType.ShortInlineR:
         {
-          myBuilder.Append(", reader.ReadInt32()");
+          builder.Append(", reader.ReadInt32()");
           break;
         }
 
         case OperandType.InlineBrTarget:
         {
-          myBuilder.Append(", reader.ReadInt32() + reader.Offset");
+          builder.Append(", reader.ReadInt32() + reader.Offset");
           break;
         }
 
         case OperandType.InlineSwitch:
         {
-          myBuilder.Append(", ReadSwitch(ref reader)");
+          builder.Append(", ReadSwitch(ref reader)");
           break;
         }
 
         case OperandType.InlineI8:
         case OperandType.InlineR:
         {
-          myBuilder.Append(", reader.ReadInt64()");
+          builder.Append(", reader.ReadInt64()");
           break;
         }
 
         case OperandType.InlineVar:
         {
-          myBuilder.Append(", reader.ReadInt16()");
+          builder.Append(", reader.ReadInt16()");
           break;
         }
 
         case OperandType.ShortInlineVar:
         case OperandType.ShortInlineI:
         {
-          myBuilder.Append(", reader.ReadByte()");
+          builder.Append(", reader.ReadByte()");
           break;
         }
 
         case OperandType.ShortInlineBrTarget:
         {
-          myBuilder.Append(", reader.ReadSByte() + reader.Offset");
+          builder.Append(", reader.ReadSByte() + reader.Offset");
           break;
         }
       }
     }
 
-    myBuilder.Append("));");
-    myBuilder.AppendLine();
+    builder.Append("));");
+    builder.AppendLine();
   }
 
   private static int? GetValue(OpCode code, ref string name)
---
+++ src/System.Reflection.Metadata.Extensions/ILReader/ILReader2.cs
@@ -0,0 +1,275 @@
+﻿namespace System.Reflection.Metadata.ILReader
+{
+  public partial class ILReaderImpl
+  {
+    public static int Count(BlobReader reader)
+    {
+      int count = 0;
+
+      while (reader.RemainingBytes > 0)
+      {
+        count ++;
+
+        switch (reader.ReadByte())
+        {
+          case 0x00: // nop
+          case 0x01: // break
+          case 0x02: // ldarg.0
+          case 0x03: // ldarg.1
+          case 0x04: // ldarg.2
+          case 0x05: // ldarg.3
+          case 0x06: // ldloc.0
+          case 0x07: // ldloc.1
+          case 0x08: // ldloc.2
+          case 0x09: // ldloc.3
+          case 0x0A: // stloc.0
+          case 0x0B: // stloc.1
+          case 0x0C: // stloc.2
+          case 0x0D: // stloc.3
+          case 0x14: // ldnull
+          case 0x15: // ldc.i4.m1
+          case 0x16: // ldc.i4.0
+          case 0x17: // ldc.i4.1
+          case 0x18: // ldc.i4.2
+          case 0x19: // ldc.i4.3
+          case 0x1A: // ldc.i4.4
+          case 0x1B: // ldc.i4.5
+          case 0x1C: // ldc.i4.6
+          case 0x1D: // ldc.i4.7
+          case 0x1E: // ldc.i4.8
+          case 0x25: // dup
+          case 0x26: // pop
+          case 0x2A: // ret
+          case 0x46: // ldind.i1
+          case 0x47: // ldind.u1
+          case 0x48: // ldind.i2
+          case 0x49: // ldind.u2
+          case 0x4A: // ldind.i4
+          case 0x4B: // ldind.u4
+          case 0x4C: // ldind.i8
+          case 0x4D: // ldind.i
+          case 0x4E: // ldind.r4
+          case 0x4F: // ldind.r8
+          case 0x50: // ldind.ref
+          case 0x51: // stind.ref
+          case 0x52: // stind.i1
+          case 0x53: // stind.i2
+          case 0x54: // stind.i4
+          case 0x55: // stind.i8
+          case 0x56: // stind.r4
+          case 0x57: // stind.r8
+          case 0x58: // add
+          case 0x59: // sub
+          case 0x5A: // mul
+          case 0x5B: // div
+          case 0x5C: // div.un
+          case 0x5D: // rem
+          case 0x5E: // rem.un
+          case 0x5F: // and
+          case 0x60: // or
+          case 0x61: // xor
+          case 0x62: // shl
+          case 0x63: // shr
+          case 0x64: // shr.un
+          case 0x65: // neg
+          case 0x66: // not
+          case 0x67: // conv.i1
+          case 0x68: // conv.i2
+          case 0x69: // conv.i4
+          case 0x6A: // conv.i8
+          case 0x6B: // conv.r4
+          case 0x6C: // conv.r8
+          case 0x6D: // conv.u4
+          case 0x6E: // conv.u8
+          case 0x76: // conv.r.un
+          case 0x7A: // throw
+          case 0x82: // conv.ovf.i1.un
+          case 0x83: // conv.ovf.i2.un
+          case 0x84: // conv.ovf.i4.un
+          case 0x85: // conv.ovf.i8.un
+          case 0x86: // conv.ovf.u1.un
+          case 0x87: // conv.ovf.u2.un
+          case 0x88: // conv.ovf.u4.un
+          case 0x89: // conv.ovf.u8.un
+          case 0x8A: // conv.ovf.i.un
+          case 0x8B: // conv.ovf.u.un
+          case 0x8E: // ldlen
+          case 0x90: // ldelem.i1
+          case 0x91: // ldelem.u1
+          case 0x92: // ldelem.i2
+          case 0x93: // ldelem.u2
+          case 0x94: // ldelem.i4
+          case 0x95: // ldelem.u4
+          case 0x96: // ldelem.i8
+          case 0x97: // ldelem.i
+          case 0x98: // ldelem.r4
+          case 0x99: // ldelem.r8
+          case 0x9A: // ldelem.ref
+          case 0x9B: // stelem.i
+          case 0x9C: // stelem.i1
+          case 0x9D: // stelem.i2
+          case 0x9E: // stelem.i4
+          case 0x9F: // stelem.i8
+          case 0xA0: // stelem.r4
+          case 0xA1: // stelem.r8
+          case 0xA2: // stelem.ref
+          case 0xB3: // conv.ovf.i1
+          case 0xB4: // conv.ovf.u1
+          case 0xB5: // conv.ovf.i2
+          case 0xB6: // conv.ovf.u2
+          case 0xB7: // conv.ovf.i4
+          case 0xB8: // conv.ovf.u4
+          case 0xB9: // conv.ovf.i8
+          case 0xBA: // conv.ovf.u8
+          case 0xC3: // ckfinite
+          case 0xD1: // conv.u2
+          case 0xD2: // conv.u1
+          case 0xD3: // conv.i
+          case 0xD4: // conv.ovf.i
+          case 0xD5: // conv.ovf.u
+          case 0xD6: // add.ovf
+          case 0xD7: // add.ovf.un
+          case 0xD8: // mul.ovf
+          case 0xD9: // mul.ovf.un
+          case 0xDA: // sub.ovf
+          case 0xDB: // sub.ovf.un
+          case 0xDC: // endfinally
+          case 0xDF: // stind.i
+          case 0xE0: // conv.u
+          case 0xF8: // prefix7
+          case 0xF9: // prefix6
+          case 0xFA: // prefix5
+          case 0xFB: // prefix4
+          case 0xFC: // prefix3
+          case 0xFD: // prefix2
+          case 0xFF: // prefixref
+            continue;
+          case 0x0E: // ldarg.s
+          case 0x0F: // ldarga.s
+          case 0x10: // starg.s
+          case 0x11: // ldloc.s
+          case 0x12: // ldloca.s
+          case 0x13: // stloc.s
+          case 0x1F: // ldc.i4.s
+            reader.ReadByte();
+            continue;
+          case 0x20: // ldc.i4
+          case 0x22: // ldc.r4
+          case 0x27: // jmp
+          case 0x28: // call
+          case 0x29: // calli
+          case 0x38: // br
+          case 0x39: // brfalse
+          case 0x3A: // brtrue
+          case 0x3B: // beq
+          case 0x3C: // bge
+          case 0x3D: // bgt
+          case 0x3E: // ble
+          case 0x3F: // blt
+          case 0x40: // bne.un
+          case 0x41: // bge.un
+          case 0x42: // bgt.un
+          case 0x43: // ble.un
+          case 0x44: // blt.un
+          case 0x6F: // callvirt
+          case 0x70: // cpobj
+          case 0x71: // ldobj
+          case 0x72: // ldstr
+          case 0x73: // newobj
+          case 0x74: // castclass
+          case 0x75: // isinst
+          case 0x79: // unbox
+          case 0x7B: // ldfld
+          case 0x7C: // ldflda
+          case 0x7D: // stfld
+          case 0x7E: // ldsfld
+          case 0x7F: // ldsflda
+          case 0x80: // stsfld
+          case 0x81: // stobj
+          case 0x8C: // box
+          case 0x8D: // newarr
+          case 0x8F: // ldelema
+          case 0xA3: // ldelem
+          case 0xA4: // stelem
+          case 0xA5: // unbox.any
+          case 0xC2: // refanyval
+          case 0xC6: // mkrefany
+          case 0xD0: // ldtoken
+          case 0xDD: // leave
+            reader.ReadInt32();
+            continue;
+          case 0x21: // ldc.i8
+          case 0x23: // ldc.r8
+            reader.ReadInt64();
+            continue;
+          case 0x2B: // br.s
+          case 0x2C: // brfalse.s
+          case 0x2D: // brtrue.s
+          case 0x2E: // beq.s
+          case 0x2F: // bge.s
+          case 0x30: // bgt.s
+          case 0x31: // ble.s
+          case 0x32: // blt.s
+          case 0x33: // bne.un.s
+          case 0x34: // bge.un.s
+          case 0x35: // bgt.un.s
+          case 0x36: // ble.un.s
+          case 0x37: // blt.un.s
+          case 0xDE: // leave.s
+            reader.ReadSByte();
+            continue;
+          case 0x45: // switch
+            for (var cases = reader.ReadUInt32(); cases > 0; cases--) reader.ReadInt32();
+            continue;
+          case 254:
+            switch (reader.ReadByte())
+            {
+              case 0x00: // arglist
+              case 0x01: // ceq
+              case 0x02: // cgt
+              case 0x03: // cgt.un
+              case 0x04: // clt
+              case 0x05: // clt.un
+              case 0x0F: // localloc
+              case 0x11: // endfilter
+              case 0x13: // volatile.
+              case 0x14: // tail.
+              case 0x17: // cpblk
+              case 0x18: // initblk
+              case 0x1A: // rethrow
+              case 0x1D: // refanytype
+              case 0x1E: // readonly.
+
+                continue;
+              case 0x06: // ldftn
+              case 0x07: // ldvirtftn
+              case 0x15: // initobj
+              case 0x16: // constrained.
+              case 0x1C: // sizeof
+                reader.ReadInt32();
+                continue;
+              case 0x09: // ldarg
+              case 0x0A: // ldarga
+              case 0x0B: // starg
+              case 0x0C: // ldloc
+              case 0x0D: // ldloca
+              case 0x0E: // stloc
+                reader.ReadInt16();
+                continue;
+              case 0x12: // unaligned.
+                reader.ReadByte();
+                continue;
+              default:
+                UnexpectedOpcode();
+                continue;
+            }
+          default:
+            UnexpectedOpcode();
+            continue;
+        }
+      }
+
+      return count;
+    }
+  }
+}
